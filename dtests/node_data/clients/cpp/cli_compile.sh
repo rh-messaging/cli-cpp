faabc86eedc03b8da68d413fb6825a61da7bfb6c
Frantisek Reznicek <freznice@redhat.com>
2015-05-05 16:16:25 +0200

    FIX: migrate from cd X ... cd - to pushd X ... popd
    GitOrigin-RevId: 5bedee70eb282ede6c67f3f5058c9ba06164dee5

--- dtests/node_data/clients/cpp/cli_compile.sh
+++ dtests/node_data/clients/cpp/cli_compile.sh
@@ -2,18 +2,18 @@
 
 basepath=$(readlink -f $(dirname $0))
 
-cd ${basepath}
+pushd ${basepath}/..
 
 # MRG/M part
-bash ${basepath}/../configlib.sh do_node_data_clients_mrgm_cpp_status
+bash ${basepath}/../../configlib.sh do_node_data_clients_mrgm_cpp_status
 if [ "$?" != "0" ]; then
-  bash ${basepath}/../configlib.sh do_node_data_clients_mrgm_init
-  bash ${basepath}/../configlib.sh do_node_data_clients_mrgm_cpp_init
+  bash ${basepath}/../../configlib.sh do_node_data_clients_mrgm_init
+  bash ${basepath}/../../configlib.sh do_node_data_clients_mrgm_cpp_init
 fi
 
 # JBoss A-MQ part
 # TODO
 
-cd -
+popd
 
 # eof
