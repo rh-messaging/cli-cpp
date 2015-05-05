69ea085e9f58c9da6e97703845bd776e4f68e046
Otavio Rodolfo Piske <opiske@redhat.com>
2015-05-05 15:41:35 +0200

    NEW: utility class to hold content types for the clients
    GitOrigin-RevId: 511973b1b6fd3886d9c55517caa64ad4c47bb9a0

---
+++ dtests/node_data/clients/cpp/src/common/ContentType.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   ContentTypes.cpp
+ * Author: opiske
+ * 
+ * Created on May 5, 2015, 3:32 PM
+ */
+
+#include "ContentType.h"
+
+const char *ContentType::TEXT_PLAIN = "text/plain" ;
+
+ContentType::ContentType() {
+}
+
+ContentType::ContentType(const ContentType& orig) {
+}
+
+ContentType::~ContentType() {
+}
+
