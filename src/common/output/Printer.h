e068761a41967ca5678d2ec146770c0cc883ee5a
Otavio Rodolfo Piske <opiske@redhat.com>
2015-05-07 11:51:34 +0200

    NEW: added new printer class, along with unit tests, so we can enable logging on the code without relying on boost::log - which does not exist on rhel5 and rhel6
    GitOrigin-RevId: 26b5f10be8197354e9d9d2a03de4df7d18bfca01

---
+++ src/common/output/Printer.h
@@ -0,0 +1,89 @@
+/* 
+ * File:   Printer.h
+ * Author: opiske
+ *
+ * Created on May 7, 2015, 9:48 AM
+ */
+
+#ifndef PRINTER_H
+#define	PRINTER_H
+
+#include <cstdio>
+
+class Printer {
+public:
+    /**
+     * Enumerates the numbering base for the output
+     */
+    enum Base {
+        HEXADECIMAL, 	/*!< Hexadecimal base */
+        DECIMAL,	/*!< Decimal base */
+        OCTAL		/*!< Octal base */
+    };
+                
+    
+    Printer();
+    Printer(FILE *stream);
+    Printer(const Printer& orig);
+    virtual ~Printer();
+    
+    /**
+     * Prints a signed integer
+     * @param val Value
+     */
+    void print(int val);
+
+    /**
+     * Prints an unsigned integer
+     * @param val Value
+     */
+    void print(unsigned int val);
+
+
+    /**
+    * Prints a signed long integer
+//    * @param val Value
+    */
+    void print(long int val);
+
+    /**
+    * Prints an unsigned long integer
+    * @param val Value
+    */
+    void print(unsigned long val);
+
+    /**
+    * Prints a double
+    * @param d Value
+    */
+    void print(double d);
+
+    /**
+    * Prints a char
+    * @param c Char
+    */
+    void print(char c);
+
+    /**
+    * Prints the address of a pointer
+    * @param ptr The address of the pointer
+    */
+    void print(const void *ptr);
+
+
+    /**
+     * Prints a char * string
+     * @param str A pointer to a null terminated string
+     *  This prevents the compiler from  casting a regular, temporary, string to
+     * a const void pointer, which would cause the print(const void *) method 
+      * to be called
+     */
+    void print(const char *str);
+private:
+    Base base;
+    FILE *stream;
+
+};
+
+#endif	/* PRINTER_H */
+
