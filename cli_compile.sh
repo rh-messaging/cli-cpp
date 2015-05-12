f85027acf04e6d0ae19fcfdeb8359327881c2410
Frantisek Reznicek <freznice@redhat.com>
2015-05-12 11:16:22 +0200

    UPD+FIX: update of cli_compile scripts to include / not-include JBoss A-MQ part, removal of extensive ../.. path walks
    GitOrigin-RevId: ebf4e65a66d665925b265f328643249f888095b8

--- cli_compile.sh
+++ cli_compile.sh
@@ -5,14 +5,18 @@ basepath=$(readlink -f $(dirname $0))
 pushd ${basepath}/..
 
 # MRG/M part
-bash ${basepath}/../../configlib.sh do_node_data_clients_mrgm_cpp_status
+bash ../configlib.sh do_node_data_clients_mrgm_cpp_status
 if [ "$?" != "0" ]; then
-  bash ${basepath}/../../configlib.sh do_node_data_clients_mrgm_init
-  bash ${basepath}/../../configlib.sh do_node_data_clients_mrgm_cpp_init
+  bash ../configlib.sh do_node_data_clients_mrgm_init
+  bash ../configlib.sh do_node_data_clients_mrgm_cpp_init
 fi
 
 # JBoss A-MQ part
-# TODO
+bash ../configlib.sh do_node_data_clients_jamq_cpp_status
+if [ "$?" != "0" ]; then
+  bash ../configlib.sh do_node_data_clients_jamq_init
+  bash ../configlib.sh do_node_data_clients_jamq_cpp_init
+fi
 
 popd
 
