f3d09a048b74de23eb274862ba6bf172c5d77956
Otavio Rodolfo Piske <opiske@redhat.com>
2015-05-14 15:59:06 +0200

    UPD: added support for disabling the messaging API but retaining the binaries - required for dtests development
    GitOrigin-RevId: 0b179a0ea81085442353c7e3b0f13d40a9ca39c4

--- dtests/node_data/clients/cpp/src/api/qpid-proton/clients/aac1_sender.cpp
+++ dtests/node_data/clients/cpp/src/api/qpid-proton/clients/aac1_sender.cpp
@@ -4,15 +4,20 @@
  *  Created on: Apr 14, 2015
  *      Author: opiske
  */
+#include <cstdlib>
 
 #include "reactor/SendingClient.h"
 
 using dtests::proton::reactor::SendingClient;
 
 int main(int argc, char** argv) {
+#ifdef QPID_PROTON_WITH_MESSAGING_API
 	SendingClient client = SendingClient();
 
 	return client.run(argc, argv);
+#else
+	return EXIT_SUCCESS;
+#endif
 }
 
 
