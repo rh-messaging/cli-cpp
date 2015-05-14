cfcf5927ec95a7ba5062d3c14b5d4b1c5d983ed8
Otavio Rodolfo Piske <opiske@redhat.com>
2015-05-14 12:34:48 +0200

    UPD: test code cleanup
    GitOrigin-RevId: a7e55b0bbab170bbfb5526b220b4ef25f77cacc8

--- src/common/output/test/ut_printer_test.cpp
+++ src/common/output/test/ut_printer_test.cpp
@@ -12,24 +12,28 @@
 #include "output/Printer.h"
 #include "TestUtils.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-	char writeString[] = "This is a test";
-	char testFileName[] = "printer-test.log";
-	
+static void createTestFile(const char *testFileName, const char *writeString) {
 	
 	FILE *writeStream = fopen(testFileName, "w+");
 	
 	if (!writeStream) {
 		fprintf(stderr, "Unable to open output file for writing");
-		return 1;
+		exit(1);
 	}
-	
+
 	Printer printer = Printer(writeStream);
 	printer.print(writeString);
 	fclose(writeStream);
+}
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+	char testFileName[] = "printer-test.log";
+	char writeString[] = "This is a test";
+	
+	createTestFile(testFileName, writeString);
 	
 	size_t len = strlen(writeString);
 	bool ret = assertFileContent(testFileName, writeString, len);
