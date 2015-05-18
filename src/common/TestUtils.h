44cf34ee7fae26fa205665ad6dbe10f88ae6572c
Otavio Rodolfo Piske <opiske@redhat.com>
2015-05-18 17:57:12 +0200

    UPD: added documentation for several common base classes
    GitOrigin-RevId: 9ced7abff52fe16492992c4079e26d847144ded2

--- src/common/TestUtils.h
+++ src/common/TestUtils.h
@@ -16,9 +16,31 @@
 #define RED "\x1B[00;31m"
 #define UNDERLINE "\x1B[04m"
 
+/**
+ * Asserts whether the content of a file matches an expected string
+ * @param filename the file name to test
+ * @param expected the expected string
+ * @param size the expected string size
+ * @return true if it matches or false otherwise
+ */
 bool assertFileContent(const char *filename, const char *expected, size_t size);
 
+/**
+ * Asserts whether the content of a string matches an expected string
+ * @param expected the expected string
+ * @param actuals the actual string
+ * @return true if it matches or false otherwise
+ */
 bool assertEquals(const char *expected, const char *actuals);
+
+
+/**
+ * Asserts whether the content of a string matches an expected string
+ * @param expected the expected string
+ * @param actuals the actual string
+ * @param size the expected string size (or the amount of bytes to test for equality)
+ * @return true if it matches or false otherwise
+ */
 bool assertEquals(const char *expected, const char *actuals, size_t size);
 
 #endif	/* TESTUTILS_H */
