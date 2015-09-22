d82ff0b4b803f5414959d86a66b14aabe6dd098e
Otavio Rodolfo Piske <opiske@redhat.com>
2015-09-22 10:58:42 +0200

    UPD: adds a GCC trick to remove the unused warning when compiling the code through configlib
    GitOrigin-RevId: 66d356eae87b5be4cb09ca5538d7fa2e4fac9a64

--- src/api/qpid/common/TimeCompatibility.h
+++ src/api/qpid/common/TimeCompatibility.h
@@ -4,10 +4,21 @@
 
 #include <qpid/sys/Time.h>
 
+
+// This trick prevents the unused warning when compiling through configlib
+// For GCC >= 4.6 it saves the previously set unused warning state. For older 
+// versions, it restores the default 'warning' behavior (default GCC level). 
+#if defined __GNUC__
+# if ((__GNUC__ == 4 && __GNUC_MINOR__ >= 6) || (__GNUC__ >= 5)
+#   pragma GCC diagnostic push
+# endif
+
+# pragma GCC diagnostic ignored "-Wunused"
+          
+
 // Duration duration_since_epoch(string in_var)
 // gets duration since Epoch
 // --------------------------------------------------------------------------
-
 static qpid::sys::Duration duration_since_epoch()
 {
 #ifdef QPIDSYSEPOCH
@@ -18,6 +29,13 @@ static qpid::sys::Duration duration_since_epoch()
   #error Either QPIDSYSEPOCH or QPIDSYSDURATIONFROMEPOCH macro has to be defined!
 #endif
 }
+
+# if ((__GNUC__ == 4 && __GNUC_MINOR__ >= 6) || (__GNUC__ >= 5)
+#   pragma GCC diagnostic pop
+# else
+# pragma GCC diagnostic warning "-Wunused"
+# endif 
+#endif // defined __GNUC__
 
 
 #endif
