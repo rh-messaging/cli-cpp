f17af9d680251724b75ab6e103758406cf5ef6fb
Otavio Rodolfo Piske <opiske@redhat.com>
2015-09-22 13:20:11 +0200

    UPD: replaces the pragma hacks with a simpler, attribute-based, solution
    GitOrigin-RevId: 8a7e76d06ad36a65b253d84057c3ccbf5969c097

--- src/api/qpid/common/TimeCompatibility.h
+++ src/api/qpid/common/TimeCompatibility.h
@@ -8,19 +8,16 @@
 // This trick prevents the unused warning when compiling through configlib
 // For GCC >= 4.6 it saves the previously set unused warning state. For older 
 // versions, it restores the default 'warning' behavior (default GCC level). 
-#if defined __GNUC__
-# if ((__GNUC__ == 4 && __GNUC_MINOR__ >= 6) || (__GNUC__ >= 5))
-#   pragma GCC diagnostic push
-# endif
-
-# pragma GCC diagnostic ignored "-Wunused"
-# endif // __GNUC__
-          
+#if defined __GNUC__ && (__GNUC__ >= 4)
+# define ATTRIBUTES __attribute__((unused))
+#else
+# define ATTRIBUTES
+#endif // __GNUC__ 
 
 // Duration duration_since_epoch(string in_var)
 // gets duration since Epoch
 // --------------------------------------------------------------------------
-static qpid::sys::Duration duration_since_epoch()
+ATTRIBUTES static qpid::sys::Duration duration_since_epoch()
 {
 #ifdef QPIDSYSEPOCH
   return(qpid::sys::Duration(qpid::sys::EPOCH, qpid::sys::now()));
@@ -30,14 +27,5 @@ static qpid::sys::Duration duration_since_epoch()
   #error Either QPIDSYSEPOCH or QPIDSYSDURATIONFROMEPOCH macro has to be defined!
 #endif
 }
-
-#if defined __GNUC__
-# if ((__GNUC__ == 4 && __GNUC_MINOR__ >= 6) || (__GNUC__ >= 5))
-#   pragma GCC diagnostic pop
-# else
-# pragma GCC diagnostic warning "-Wunused"
-# endif 
-#endif // defined __GNUC__
-
 
 #endif
