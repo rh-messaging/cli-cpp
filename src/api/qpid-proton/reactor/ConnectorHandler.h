ea38ae325659f3b116399062c5fa711a70bcd436
Otavio Rodolfo Piske <opiske@redhat.com>
2015-10-14 15:39:28 +0200

    NEW: added a connector-specific messaging handler
    GitOrigin-RevId: 93de98bb7409fe18ea6ba4bd47789237e5e2a0d5

---
+++ src/api/qpid-proton/reactor/ConnectorHandler.h
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ConnectorHandler.h
+ * Author: opiske
+ *
+ * Created on October 14, 2015, 3:33 PM
+ */
+
+#ifndef CONNECTORHANDLER_H
+#define CONNECTORHANDLER_H
+
+#include "SimpleMessagingHandler.h"
+
+#include <proton/messaging_handler.hpp>
+
+namespace dtests {
+namespace proton {
+namespace reactor {
+
+class ConnectorHandler: public SimpleMessagingHandler {
+public:
+    ConnectorHandler(const string &url);
+    
+    virtual ~ConnectorHandler();
+private:
+
+    typedef SimpleMessagingHandler super;
+};
+
+} /* namespace reactor */
+} /* namespace proton */
+} /* namespace dtests */
+
+#endif /* CONNECTORHANDLER_H */
+
