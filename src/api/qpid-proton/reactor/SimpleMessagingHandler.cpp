8b57eb24c81592284f943aba19ec7e6fee2519f9
Otavio Rodolfo Piske <opiske@redhat.com>
2015-10-14 15:01:53 +0200

    UPD: added additional debugging log for supporting development
    GitOrigin-RevId: 4f4cfca522fd6bda6b17fd3904e01a91de6fa5c3

--- src/api/qpid-proton/reactor/SimpleMessagingHandler.cpp
+++ src/api/qpid-proton/reactor/SimpleMessagingHandler.cpp
@@ -16,13 +16,17 @@ namespace dtests {
 namespace proton {
 namespace reactor {
 
+    
+using namespace dtests::common;
+using namespace dtests::common::log;
 
+Logger SimpleMessagingHandler::logger = LoggerWrapper::getLogger();
 
 SimpleMessagingHandler::SimpleMessagingHandler(const string &url_str)
 	: super(),
 	broker_url(url_str)
 {
-
+	logger(debug) << "Creating a handler for " << url_str;
 }
 
 SimpleMessagingHandler::~SimpleMessagingHandler() {
@@ -31,30 +35,40 @@ SimpleMessagingHandler::~SimpleMessagingHandler() {
 
 
 void SimpleMessagingHandler::on_start(event &e) {
+	logger(debug) << "Starting messaging handler";
 	connection &conn = e.container().connect(broker_url);
-
-	conn.create_sender(broker_url.path());
+	logger(debug) << "Connected to " << broker_url.path();
+	
 }
 
 void SimpleMessagingHandler::on_sendable(event &e) {
+	connection &conn = e.connection();
+	
+	logger(debug) << "Creating sender for " << broker_url.path();
+	conn.create_sender(broker_url.path());
+	
+	logger(debug) << "Ready to send";
+	/*
 	message m;
 
 	m.body("Hello World!");
 	
 	e.sender().send(m);
 	e.sender().close();
+	 */
 }
 
 void SimpleMessagingHandler::on_message(event &e) {
-
+	logger(debug) << "Ready to receive";
 }
 
 void SimpleMessagingHandler::on_accepted(event &e) {
+	logger(debug) << "Accepted";
 	e.connection().close();
 }
 
 void SimpleMessagingHandler::on_connection_closed(event &e) {
-
+	logger(debug) << "Closed";
 }
 
 } /* namespace reactor */
