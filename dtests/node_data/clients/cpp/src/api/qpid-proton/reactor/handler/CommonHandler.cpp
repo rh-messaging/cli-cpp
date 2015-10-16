b0dd46ca1615e76c20ca042ed4324497b39d8b09
Otavio Rodolfo Piske <opiske@redhat.com>
2015-10-16 10:45:16 +0200

    UPD: removed methods that should be implemented in the specialized classes
    GitOrigin-RevId: 562fa7cda252190548a14454d07bd6ea106e8ef4

--- dtests/node_data/clients/cpp/src/api/qpid-proton/reactor/handler/CommonHandler.cpp
+++ dtests/node_data/clients/cpp/src/api/qpid-proton/reactor/handler/CommonHandler.cpp
@@ -7,56 +7,27 @@
 
 #include "CommonHandler.h"
 
-using proton::messaging_handler;
-using proton::message;
-using proton::event;
-using proton::url;
-
 namespace dtests {
 namespace proton {
 namespace reactor {
 
-    
 using namespace dtests::common;
 using namespace dtests::common::log;
 
 Logger CommonHandler::logger = LoggerWrapper::getLogger();
 
 CommonHandler::CommonHandler(const string &url_str)
-	: super(),
-	broker_url(url_str)
+    : super(),
+    broker_url(url_str)
 {
-	logger(debug) << "Creating a handler for " << url_str;
+    logger(debug) << "Creating a handler for " << url_str;
 }
 
-CommonHandler::~CommonHandler() {
+CommonHandler::~CommonHandler()
+{
 
 }
 
-
-void CommonHandler::on_start(event &e) {
-	logger(debug) << "Starting messaging handler";
-	connection &conn = e.container().connect(broker_url);
-	logger(debug) << "Connected to " << broker_url.path();
-}
-
-void CommonHandler::on_sendable(event &e) {
-	connection &conn = e.connection();
-	
-	logger(debug) << "Creating sender for " << broker_url.path();
-	conn.create_sender(broker_url.path());
-	
-	logger(debug) << "Ready to send";
-}
-
-void CommonHandler::on_message(event &e) {
-	logger(debug) << "Ready to receive";
-}
-
-void CommonHandler::on_accepted(event &e) {
-	logger(debug) << "Accepted";
-	e.connection().close();
-}
 
 } /* namespace reactor */
 } /* namespace proton */
