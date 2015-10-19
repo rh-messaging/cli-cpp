cf1e44335794f77eff04fe4558fc90dd7c4cf90c
Otavio Rodolfo Piske <opiske@redhat.com>
2015-10-19 16:48:05 +0200

    NEW: added receiving client and handlers stub code for the reactive API
    GitOrigin-RevId: c9fe50fe2443668caa2b80d8416cc05b8648e8e7

---
+++ src/api/qpid-proton/reactor/ReceivingClient.h
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ReceivingClient.h
+ * Author: opiske
+ *
+ * Created on October 19, 2015, 4:25 PM
+ */
+
+#ifndef RECEIVINGCLIENT_H
+#define RECEIVINGCLIENT_H
+
+#include <proton/message.hpp>
+#include <proton/container.hpp>
+
+#include "ModernClient.h"
+#include "handler/SenderHandler.h"
+#include "options/modern/SenderOptionsParser.h"
+#include "options/OptionsHelper.h"
+
+using proton::message;
+
+namespace dtests {
+namespace proton {
+namespace reactor {
+
+typedef void (message::*string_setter)(const string &);
+
+using dtests::proton::common::ModernClient;
+
+class ReceivingClient : public ModernClient {
+  public:
+    ReceivingClient();
+    virtual ~ReceivingClient();
+    
+    virtual int run(int argc, char **argv) const;
+    
+  private:
+    typedef ModernClient super;
+
+    void setMessageOptions(const OptionsSetter &setter, message &msg) const;
+
+};
+
+} /* namespace reactor */
+} /* namespace proton */
+} /* namespace dtests */
+
+#endif /* RECEIVINGCLIENT_H */
+
