a948bae9d88a43de0d2c6999b44e6463034a8871
Otavio Rodolfo Piske <opiske@redhat.com>
2015-10-19 16:48:05 +0200

    NEW: added receiving client and handlers stub code for the reactive API
    GitOrigin-RevId: c9fe50fe2443668caa2b80d8416cc05b8648e8e7

---
+++ dtests/node_data/clients/cpp/src/api/qpid-proton/reactor/ReceivingClient.cpp
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ReceivingClient.cpp
+ * Author: opiske
+ * 
+ * Created on October 19, 2015, 4:25 PM
+ */
+
+#include "ReceivingClient.h"
+
+namespace dtests {
+namespace proton {
+namespace reactor {
+
+
+ReceivingClient::ReceivingClient()
+    : super()
+{
+}
+
+ReceivingClient::~ReceivingClient()
+{
+}
+
+void ReceivingClient::setMessageOptions(const OptionsSetter &setter, message &msg) const {
+
+}
+
+int ReceivingClient::run(int argc, char **argv) const {
+
+}
+
+} /* namespace reactor */
+} /* namespace proton */
+} /* namespace dtests */
