6bb7402b80da31ad8474b8c56836db35f89105e8
Otavio Rodolfo Piske <opiske@redhat.com>
2015-11-02 11:16:16 +0100

    UPD: sanitized MessageHeader class namespace
    GitOrigin-RevId: a526d1702d38c05baa13c9e8feb3393ef37db458

--- src/common/exceptions/IOException.h
+++ src/common/exceptions/IOException.h
@@ -29,7 +29,7 @@ class IOException : public BaseException {
     typedef BaseException super;
 };
 
-}
-}
+} /* namespace common */
+} /* namespace dtests */
 
 #endif /* DTESTS_COMMON_IOEXCEPTION_H_ */
