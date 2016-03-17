f1f09a245db4000a050598f5891d0298fb726666
Otavio Rodolfo Piske <opiske@redhat.com>
2016-03-17 13:41:55 +0100

    UPD: adjusted the options setting code for Qpid Proton 0.13
    GitOrigin-RevId: b5e7d1cbbdfb7c624cd4e2d1bdb17f4d85ba4833

--- src/api/qpid-proton/reactor/TypeUtils.h
+++ src/api/qpid-proton/reactor/TypeUtils.h
@@ -8,6 +8,7 @@ using proton::message;
 using proton::message_id;
 using proton::amqp_timestamp;
 using proton::value;
+using proton::duration;
 
 namespace dtests {
 namespace proton {
@@ -39,6 +40,22 @@ typedef void (message::*value_setter)(const value &);
  */
 typedef void (message::*id_setter)(const message_id &);
 
+
+/**
+ * A proton::duration setter type
+ */
+typedef void (message::*duration_setter)(duration);
+
+
+/**
+ * A proton::duration setter type
+ */
+typedef void (message::*boolean_setter)(bool);
+
+/**
+ * A proton::data setter type
+ */
+typedef void (message::*uint_setter)(uint8_t);
 
 
 
