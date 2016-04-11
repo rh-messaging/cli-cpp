6a12048aa6b9d7a482a3f21ee4a669a996d3d181
Otavio Rodolfo Piske <opiske@redhat.com>
2016-04-11 11:21:38 +0200

    NEW: updated the sending code to be able to handle timer events
    GitOrigin-RevId: 7442cd3bea6b9798881b7c59f67835ba419639ed

--- src/api/qpid-proton/reactor/handler/SenderHandler.cpp
+++ src/api/qpid-proton/reactor/handler/SenderHandler.cpp
@@ -20,11 +20,13 @@ namespace reactor {
 using namespace dtests::common;
 using namespace dtests::common::log;
 
-SenderHandler::SenderHandler(const string &url)
+SenderHandler::SenderHandler(const string &url, int timeout)
     : super(url),
     count(1),
     sent(0),
     confirmedSent(0),
+    timeoutTask(NULL), 
+    timer(timeout, "timeout"),
     m()
 {
 
@@ -39,6 +41,10 @@ SenderHandler::~SenderHandler()
 void SenderHandler::on_start(event &e)
 {
     logger(debug) << "Starting messaging handler";
+    
+    logger(trace) << "Setting up timer";
+    task t = e.container().schedule(500);
+    timeoutTask = &t;
 
     logger(debug) << "Creating a sender";
     e.container().open_sender(broker_url);
@@ -52,7 +58,6 @@ void SenderHandler::on_sendable(event &e)
 
     if (credit == 0) {
         logger(warning) << "There not enough credit to send messages";
-
     }
 
     logger(debug) << "The handler has enough credit to send " << credit
@@ -61,11 +66,14 @@ void SenderHandler::on_sendable(event &e)
             << (sent > 1 ? "s" : "" );
 
     while (credit > 0 && sent < count) {
-        logger(trace) << "Sending messages through the link";      
+        logger(trace) << "Sending messages through the link";
+        
+        m.id(sent);
         s.send(m);
         logger(trace) << "Sent message: " << m.body().as_string();
          
         sent++;
+        timer.reset();
     }    
 }
 
@@ -75,16 +83,14 @@ void SenderHandler::on_delivery_accept(event& e)
     logger(trace) << "Message accepted. Now obtaining the connection reference object";
     connection conn = e.connection();
 
-    do {
-        confirmedSent++;
-        logger(debug) << "Closing connection";
-        
-        logger(debug) << "Connection closed";
-    } while (confirmedSent < count);
+    logger(trace) << "Confirmed message delivery";
+    confirmedSent++;
     
-    logger(trace) << "Closing the sender after sending " << confirmedSent 
+    if (confirmedSent == count) { 
+        logger(trace) << "Closing the sender after sending " << confirmedSent 
             << " message" << (confirmedSent > 1 ? "s" : "" );
-    conn.close();
+        conn.close();
+    }
 }
 
 void SenderHandler::on_transport_close(event &e) {
@@ -102,12 +108,46 @@ void SenderHandler::on_connection_close(event &e)
 {
     logger(debug) << "Stopping reactor";
     logger(debug) << "Event name: " << e.name();
+    timeoutTask = NULL;
 }
 
 void SenderHandler::on_connection_error(event &e)
 {
     logger(error) << "Failed to connect to " << broker_url.host_port();
     logger(debug) << "Event name: " << e.name();
+    
+     if (!timeoutTask) {
+        logger(debug) << "Quiescing, therefore ignoring event: " << e.name();
+
+        return;
+    }
+    
+    timeoutTask = NULL;
+}
+
+
+void SenderHandler::on_timer(event &e)
+{
+    if (!timeoutTask) {
+        logger(debug) << "Quiescing, therefore ignoring event: " << e.name();
+
+        return;
+    }
+
+    if (timer.isExpired()) {
+        logger(info) << "Timed out";
+
+        /**
+         * TODO: this is, certainly, a bad way to exit. However, upstream does 
+         * not yet have a stable interface for timers. This should be fixed in 
+         * the future.
+         */
+        exit(1);
+    } else {
+        timer--;
+        logger(debug) << "Waiting ...";
+        e.container().schedule(1000);
+    }
 }
 
 void SenderHandler::setCount(int count)
