8b9e514871c79bcef294d99aa4ef98be23deb321
Radim Kubis <rkubis@redhat.com>
2016-11-22 16:22:04 +0100

    UPD(clients): Deleted unnecessary comments for authentication
      Relates:
        MSGQE-589
        MSGQE-627
        MSGQE-629
        MSGQE-639
    
    GitOrigin-RevId: 9d5193cf5b3593355b933fc7692dde72da8094cb

--- src/api/qpid-proton/reactor/handler/ReceiverHandler.cpp
+++ src/api/qpid-proton/reactor/handler/ReceiverHandler.cpp
@@ -72,8 +72,8 @@ void ReceiverHandler::on_container_start(container &c)
                 c.client_connection_options()
                     .user(user)
                     .password(password)
-                    .sasl_enabled(true) // TODO: CLI parameter???
-                    .sasl_allow_insecure_mechs(true) // TODO: CLI parameter???
+                    .sasl_enabled(true)
+                    .sasl_allow_insecure_mechs(true)
                     .sasl_allowed_mechs(sasl_mechanisms)
         );
     } else {
@@ -82,8 +82,8 @@ void ReceiverHandler::on_container_start(container &c)
                 c.client_connection_options()
                     .user(user)
                     .password(password)
-                    .sasl_enabled(true) // TODO: CLI parameter???
-                    .sasl_allow_insecure_mechs(true) // TODO: CLI parameter???
+                    .sasl_enabled(true)
+                    .sasl_allow_insecure_mechs(true)
                     .sasl_allowed_mechs(sasl_mechanisms)
         );
     }
