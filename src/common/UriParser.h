d03d76139942e1ea2bb29e8cd6c7514356c6c8a8
Radim Kubis <rkubis@redhat.com>
2017-02-24 10:59:52 +0100

    NEW(clients): Simple UriParser implemented for C++ clients
    ~~
    Cause:
      C++ reactive clients do not expect username and password in broker-url parameter,
      but in separate parameters.
    Consequence:
      C++ reactive clients are not able to use username and password from broker-url
      parameter for connection with broker and test fails.
    Fix:
      Implementation of our own URI parser for C++ clients and his usage in affected
      C++ clients.
    Result:
      Affected C++ clients are able to connect to broker with credentials provided in
      broker-url parameter now.
    ~~
    Relates:
      MSGQE-1913 Watch and review reactive C++ results in CI
    GitOrigin-RevId: a7fe8a83e274850f3f515ad9f6d848a30e068797

---
+++ src/common/UriParser.h
@@ -0,0 +1,126 @@
+/*
+ * UriParser.h
+ *
+ *  Created on: Feb 23, 2017
+ *      Author: rkubis
+ */
+
+#ifndef URIPARSER_H
+#define URIPARSER_H
+
+#include <string>
+
+namespace dtests {
+namespace common {
+
+using std::string;
+
+/**
+ * Implementation of URI parser.
+ */
+class UriParser {
+  public:
+    /**
+     * Constructor of empty UriParser object
+     */
+    UriParser();
+
+    /**
+     * Constructor of UriParser object with parsed input URI
+     *
+     * @param uri input URI to parse
+     */
+    UriParser(const string uri);
+
+    /**
+     * Destructor of UriParser object
+     */
+    virtual ~UriParser();
+
+    /**
+     * Parse input URI
+     *
+     * @param uri input URI to parse
+     */
+    void parse(const string uri);
+
+    /**
+     * Getter for full input URI
+     *
+     * @return full input URI
+     */
+    string getUri();
+
+    /**
+     * Getter for user parsed from input URI
+     *
+     * @return user parsed from input URI
+     */
+    string getUser();
+
+    /**
+     * Getter for password parsed from input URI
+     *
+     * @return password parsed from input URI
+     */
+    string getPassword();
+
+    /**
+     * Getter for host parsed from input URI
+     *
+     * @return host parsed from input URI
+     */
+    string getHost();
+
+    /**
+     * Getter for port parsed from input URI
+     *
+     * @return port parsed from input URI
+     */
+    string getPort();
+
+    /**
+     * Getter for path parsed from input URI
+     *
+     * @return path parsed from input URI
+     */
+    string getPath();
+
+  protected:
+    /**
+     * Full input URI
+     */
+    string uri;
+
+    /**
+     * User parsed from input URI
+     */
+    string user;
+
+    /**
+     * Password parsed from input URI
+     */
+    string password;
+
+    /**
+     * Host parsed from input URI
+     */
+    string host;
+
+    /**
+     * Port parsed from input URI
+     */
+    string port;
+
+    /**
+     * Path parsed from input URI
+     */
+    string path;
+
+};
+
+} /* namespace common */
+} /* namespace dtests */
+
+#endif /* URIPARSER_H */
+
