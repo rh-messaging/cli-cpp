f45ffcabe8763684827471e6aec7ca9062b9d47c
Radim Kubis <rkubis@redhat.com>
2017-03-15 09:53:02 +0100

    NEW(clients): New module for aac3 message properties
    Relates:
      MSGQE-1913 Review and update reactive C++ client based on results in CI
    GitOrigin-RevId: 974feaf65b986571d3e063212e24f5122ea12de0

---
+++ src/common/options/modern/StringAppendCallback.cpp
@@ -0,0 +1,60 @@
+/*
+ * StringAppendCallback.cpp
+ *
+ *  Created on: Mar 8, 2017
+ *      Author: rkubis
+ */
+
+#include "StringAppendCallback.h"
+
+using namespace dtests::common;
+
+StringAppendCallback::StringAppendCallback() :
+    str("")
+{
+}
+
+StringAppendCallback::~StringAppendCallback()
+{
+    // TODO Auto-generated destructor stub
+}
+    
+void StringAppendCallback::operator()(const optparse::Option &option, const string &opt, const string &val, const optparse::OptionParser &parser)
+{
+  append(val);
+}
+
+void StringAppendCallback::append(const string &s)
+{
+    if(str.length() > 0) {
+        str.append(";");
+    }
+
+    str.append(s);
+}
+
+vector<string> StringAppendCallback::getStrings() const
+{
+    vector<string> strings;
+
+    string::size_type colon_position = str.find(";");
+    string::size_type start = 0;
+    string::size_type length;
+
+    if (colon_position != string::npos) {
+        while(colon_position != string::npos) {
+            colon_position = str.find(";", start);
+
+            length = (colon_position - start);
+
+            strings.push_back(str.substr(start, length));
+            
+            start = (colon_position + 1);
+        }
+    } else {
+        strings.push_back(str);
+    }
+
+    return strings;
+}
+
