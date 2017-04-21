3667068bc6e82fb815d6d424a21463c8723b1c01
Radim Kubis <rkubis@redhat.com>
2017-04-21 09:32:31 +0200

    NEW(clients): Added interop message logging for aac3 clients, BUT list&map content is not supported for now
    Relates:
      MSGQE-2646 Update message formater of AMQP aac3 client
    GitOrigin-RevId: d8e3b6fb155ea134090607ec742978ffffb74795

--- src/common/formatter/DictFormatter.h
+++ src/common/formatter/DictFormatter.h
@@ -8,7 +8,19 @@
 #ifndef DTESTS_COMMON_DICTFORMATTER_H
 #define DTESTS_COMMON_DICTFORMATTER_H
 
+#include <proton/message.hpp>
+#include <proton/message_id.hpp>
+#include <proton/scalar.hpp>
+#include <proton/value.hpp>
+
 #include "AbstractFormatter.h"
+
+using proton::message;
+using proton::message_id;
+using proton::scalar;
+using proton::get;
+using proton::value;
+using proton::type_id;
 
 namespace dtests {
 namespace common {
@@ -24,6 +36,14 @@ class DictFormatter : public AbstractFormatter {
     virtual ~DictFormatter();
 
     void printMessage(const AbstractDecoder *decoder, Writer *writer) const;
+    std::string formatBool(bool b);
+    std::string formatBool(scalar b);
+    std::string formatString(std::string s, bool remove_id_prefix = false);
+    std::string formatList(const std::list <value>l);
+    std::string formatMap(const std::map <std::string, value>m);
+    std::string formatProperties(message::property_map p);
+    std::string getValue(const value &v);
+    void printMessageInterop(const message &m);
 
   private:
     typedef AbstractFormatter super;
