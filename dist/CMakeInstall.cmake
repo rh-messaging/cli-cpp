c34f15ca1982d96c3c6189547b3746e54135226d
Jiri Danek <jdanek@redhat.com>
2017-09-16 20:18:28 +0200

    UPD(aac3) enable out-of-source build
    GitOrigin-RevId: 869db67504a44ffa19ead5f723923d9f60078b11

--- dist/CMakeInstall.cmake
+++ dist/CMakeInstall.cmake
@@ -2,12 +2,12 @@
 # This macro is used to install the headers
 ########
 macro(InstallHeaders path dest)
-	IF (NOT EXISTS ${CMAKE_BINARY_DIR}/${path})
-		MESSAGE(FATAL_ERROR "Directory ${CMAKE_BINARY_DIR}/${path} does not exist")
-	ENDIF (NOT EXISTS ${CMAKE_BINARY_DIR}/${path})
+	IF (NOT EXISTS ${CMAKE_SOURCE_DIR}/${path})
+		MESSAGE(FATAL_ERROR "Directory ${CMAKE_SOURCE_DIR}/${path} does not exist")
+	ENDIF (NOT EXISTS ${CMAKE_SOURCE_DIR}/${path})
 	
-	MESSAGE(STATUS "Directory ${CMAKE_BINARY_DIR}/${path} added to distribution")
-	FILE(GLOB HEADERLIST ${CMAKE_BINARY_DIR}/${path}/*.h*)
+	MESSAGE(STATUS "Directory ${CMAKE_SOURCE_DIR}/${path} added to distribution")
+	FILE(GLOB HEADERLIST ${CMAKE_SOURCE_DIR}/${path}/*.h*)
 	FOREACH(file ${HEADERLIST})
 		INSTALL(
 			FILES ${file}
