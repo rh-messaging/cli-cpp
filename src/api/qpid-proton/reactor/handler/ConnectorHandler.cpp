eacbda9bf323ad863807a9342370aee0ecd1f179
Jiri Danek <jdanek@redhat.com>
2017-09-17 09:28:19 +0200

    HOTFIX(aac3) calling container.stop seems to deadlock too
    Apparently, not even container.stop can be called from scheduled taks.
    This commit replaces that with exit(EXIT_SUCCESS);
    
    GitOrigin-RevId: 2314d57610da0d87a8b2463a3abbe6a920eb2e0f

--- src/api/qpid-proton/reactor/handler/ConnectorHandler.cpp
+++ src/api/qpid-proton/reactor/handler/ConnectorHandler.cpp
@@ -59,7 +59,7 @@ void ConnectorHandler::timerEvent() {
     if (timer.isExpired()) {
         logger(info) << "Timed out";
 
-        senderObj.container().stop();
+        exit(EXIT_SUCCESS);
     } else {
         timer--;
         logger(debug) << "Waiting ...";
